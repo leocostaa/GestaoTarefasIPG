033e93a0738c906738ea8cb8d1cc015e728f6c07
Hugo <amaralhugo30@users.noreply.github.com>
2019-11-26 15:52:03 +0000

    Layout aplicado a todas as páginas.

---
+++ Models/Setor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Setor
+    {
+        [Required]
+        public int SetorId { get; set; }
+        [Required]
+        public String Nome { get; set; }
+        [Required]
+        public String Local { get; set; }
+    }
+}
