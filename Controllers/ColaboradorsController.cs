2bd153031cb10bfb6a108653ae227229541b1723
leocostaa <32634126+leocostaa@users.noreply.github.com>
2019-11-30 20:47:53 +0000

    Criação das migrations, do ColaboradorController e a view Colaborador

---
+++ Controllers/ColaboradorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestaoTarefasIPG.Data;
+using GestaoTarefasIPG.Models;
+
+namespace GestaoTarefasIPG.Controllers
+{
+    public class ColaboradorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ColaboradorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Colaboradors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Colaborador.ToListAsync());
+        }
+
+        // GET: Colaboradors/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var colaborador = await _context.Colaborador
+                .FirstOrDefaultAsync(m => m.ColaboradorId == id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+
+            return View(colaborador);
+        }
+
+        // GET: Colaboradors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Colaboradors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Nome,ColaboradorId,Profissao,DataNascimento,email")] Colaborador colaborador)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(colaborador);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(colaborador);
+        }
+
+        // GET: Colaboradors/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var colaborador = await _context.Colaborador.FindAsync(id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+            return View(colaborador);
+        }
+
+        // POST: Colaboradors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Nome,ColaboradorId,Profissao,DataNascimento,email")] Colaborador colaborador)
+        {
+            if (id != colaborador.ColaboradorId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(colaborador);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ColaboradorExists(colaborador.ColaboradorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(colaborador);
+        }
+
+        // GET: Colaboradors/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var colaborador = await _context.Colaborador
+                .FirstOrDefaultAsync(m => m.ColaboradorId == id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+
+            return View(colaborador);
+        }
+
+        // POST: Colaboradors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var colaborador = await _context.Colaborador.FindAsync(id);
+            _context.Colaborador.Remove(colaborador);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ColaboradorExists(int id)
+        {
+            return _context.Colaborador.Any(e => e.ColaboradorId == id);
+        }
+    }
+}
---
+++ Data/Migrations/20191130183853_Colaborador.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestaoTarefasIPG.Data.Migrations
+{
+    public partial class Colaborador : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Colaborador",
+                columns: table => new
+                {
+                    ColaboradorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Profissao = table.Column<int>(nullable: false),
+                    DataNascimento = table.Column<int>(nullable: false),
+                    email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colaborador", x => x.ColaboradorId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Colaborador");
+        }
+    }
+}
