74525764f955d03fbeca6865f31a1a397a1aa562
hugofigueira0 <hugofigueira0@gmail.com>
2019-12-04 14:54:37 +0000

    Criação do modelo e controlador cargos

---
+++ Models/Cargos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefasIPG.Models
+{
+    public class Cargos
+    {
+        [Required]
+        public int CargosId { get; set; }
+        
+        [Required]
+        public string Nome { get; set; }
+        
+        [Required]
+        public string Superior { get; set; }
+
+
+    }
+}
